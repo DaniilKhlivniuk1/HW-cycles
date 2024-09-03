f5d6ab56fbf2a16d6d783911e9bcb4035c8ffd14
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2024-09-03 15:37:34 +0300

    improve

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-let count = 1;
-while (count <= 10) {
-  console.log(count);
-  count += 1;
+// 1
+let numb = 1;
+while (numb <= 10) {
+  console.log(numb);
+  numb += 1;
 }
-
+// 2
 for (let i = 2; i <= 20; i++) {
   if (!(i % 2 === 0)) {
     continue;
   }
   console.log(i);
 }
-
+// 3
 for (let i = 0; i <= 10; i++) {
   console.log(`7 * ${i} =`, 7 * i);
 }
-
+// 4
 const numbersTo5 = [1, 2, 3, 4, 5];
 console.log(numbersTo5);
 let i = 0;
@@ -22,7 +23,7 @@ while (i <= 4) {
   console.log(numbersTo5[i]);
   i += 1;
 }
-
+// 5
 const numbersTo10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (let i = 0; i < 10; i++) {
   if (numbersTo10[i] === 7) {
@@ -30,7 +31,7 @@ for (let i = 0; i < 10; i++) {
   }
   console.log(numbersTo10[i]);
 }
-
+// 6
 console.log("");
 const n = Number(prompt("Введіть якесь число:"));
 for (let i = 0; i < 1000000; i++) {
@@ -39,20 +40,12 @@ for (let i = 0; i < 1000000; i++) {
     break;
   }
 }
-
- console.log("");
- let k = 1;
- while (k < 20) {
-     if (k % 3 === 0) {
-         continue;
-     }
-     console.log(k);
-     k += 1;
- }
- while (k < 20) {
-   if (k % 3 === 0) {
-       continue;
-   }
-   console.log(k);
-   k += 1;
- }
+// 7
+let k = 0;
+while (k < 20) {
+  k += 1;
+  if (k % 3 === 0) {
+    continue;
+  }
+  console.log(k);
+}
